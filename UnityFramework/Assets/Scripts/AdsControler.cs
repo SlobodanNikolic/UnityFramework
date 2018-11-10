611e60d712cb1dae45b2eae734fdca3125b10de9
BigMac <cobibitza@gmail.com>
2018-11-10 12:30:17 +0100

    Radi AdMob iOS Build

--- UnityFramework/Assets/Scripts/AdsControler.cs
+++ UnityFramework/Assets/Scripts/AdsControler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 using UnityEngine.Events;
+using GoogleMobileAds.Api;
+
 
 /// <summary>
 /// Klasa koja kontrolise UnityAds servis i prikazivanje reklama
@@ -21,10 +23,35 @@ public class AdsControler : MonoBehaviour
     /// </summary>
     public List<UnityEvent> rewardedEvents;
 
+    /// <summary>
+    /// AdMob reklame - promenljive
+    /// </summary>
+    private InterstitialAd interstitial;
+    public string AdMobInterIdiOS;
+    public string AdMobInterIdAnd;
+    public string AdMobRewardedIdiOS;
+    public string AdMobRewardedIdAnd;
+
+    public string AdMobInterIdiOSTEST = "ca-app-pub-3940256099942544/4411468910";
+    public string AdMobInterIdAndTEST = "ca-app-pub-3940256099942544/1033173712";
+    public string AdMobRewardedIdiOSTEST = "ca-app-pub-3940256099942544/1712485313";
+    public string AdMobRewardedIdAndTEST = "ca-app-pub-3940256099942544/5224354917";
 
     public void Start()
     {
+        /// <summary>
+        /// Podesavanje za Admob
+        /// </summary>
+#if UNITY_ANDROID
+            string appId = "ca-app-pub-3405361179188325~5469892951";
+#elif UNITY_IPHONE
+        string appId = "ca-app-pub-3405361179188325~7905033343";
+#else
+            string appId = "unexpected_platform";
+#endif
 
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
     }
 
     /// <summary>
@@ -153,6 +180,73 @@ public class AdsControler : MonoBehaviour
         }
     }
 
+    public void RequestAdMobInterstitial()
+    {
+#if UNITY_ANDROID
+        string adUnitId = "ca-app-pub-3405361179188325/5278321267";
+#elif UNITY_IPHONE
+        string adUnitId = AdMobInterIdiOSTEST;
+#else
+        string adUnitId = "unexpected_platform";
+#endif
 
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        Debug.Log("Interstitial requested");
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+    }
+
+
+    public void ShowAdMobInterstitial(){
+        Debug.Log("Show ad mob inter");
+        if (this.interstitial.IsLoaded())
+        {
+            Debug.Log("Showing inter");
+            this.interstitial.Show();
+        }
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+        interstitial.Destroy();
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
 }
 
